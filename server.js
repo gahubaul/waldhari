9d84bee4e4df74384b9810e6d527bf280292c59f
gahubaul <gautier.hubault@gmail.com>
2018-09-27 03:37:52 +0200

    modif plain html

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(req, res) {
     console.log(__dirname)
     console.log(__dirname + '/public')
-    res.setHeader('Content-Type', 'text/plain') // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
+    res.setHeader('Content-Type', 'text/html') // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
     res.render('page.ejs')
 });
 
 app.get('/test', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html')
     res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
 });
 
 app.get('/home', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html')
     res.send('Hé ho, c\'est privé ici !');
 });
 
@@ -37,6 +37,7 @@ app.post('/upload', function(req, res) {
     console.log(__dirname)
     sampleFile.mv(__dirname + '/Notes-unparsed/' + req.files.sampleFile.name, function(err) {
         console.log({err})
+        res.setHeader('Content-Type', 'text/html')
         if (err)
             return res.status(500).send(err)
         res.render('page_upload.ejs')
@@ -44,6 +45,7 @@ app.post('/upload', function(req, res) {
 })
 
 app.get('/read', (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
     fs.readFile(__dirname + '/Notes-unparsed/' + 'Notes Sapiens.html', 'utf8', function(err, html){
         if (!err) {
             const dom = parser.parseFromString(html, 'text/html');
@@ -59,7 +61,7 @@ app.get('/read', (req, res) => {
 })
 
 app.use(function(req, res, next){
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html')
     res.status(404).send('Page introuvable !');
 });
 
