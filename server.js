c8c52ad719fa9331b6cc71a2d123e67e3d4b456a
gahubaul <gautier.hubault@gmail.com>
2018-09-27 03:33:39 +0200

    modif gautier

--- server.js
+++ server.js
@@ -4,59 +4,63 @@ const fileUpload = require('express-fileupload');
 const upload = multer()
 const app = express()
 const port = process.env.PORT || 8080;
+const DomParser = require('dom-parser');
+const parser = new DomParser();
+const fs = require('file-system')
 
 
-function generateHTML (res) {
-    const titi = {
-        status: '',
-        error: '',
-        successs: ''
-    }
-
-    if (res === 'salut')
-    {
-        titi.status = 200
-        titi.successs = 'Tout a fonctionné'
-        titi.error = null
-    } else {
-        titi.status = 404
-        titi.successs = null
-        titi.error = 'DATA NOT FOUND'
-    }
-    return (titi)
-}
 
 app.use(fileUpload())
 app.use(express.static(__dirname + '/public'))
 
-app.all('*', (req, res) => {
-    const path = req.params["0"]
-    const query = req.query
+app.get('/', function(req, res) {
+    console.log(__dirname)
+    console.log(__dirname + '/public')
+    res.setHeader('Content-Type', 'text/plain') // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
+    res.render('page.ejs')
+});
 
-    switch (path) {
-        case '/':
-            console.log('main page') // ajouter app use pour dire ou se trouve les static de la page. Ici on vas chercher dans le dossier public
-            res.render('page.ejs')
-            break;
-         case '/test':
-            const toto = generateHTML(query.var1)
-            res.send(toto)
-            break;
-        case '/upload':
-            if (!req.files)
-                return res.status(400).send('No files were uploaded.')
-            let sampleFile = req.files.sampleFile
-            sampleFile.mv(__dirname + '/Notes-unparsed/' + req.files.sampleFile.name, function(err) {
-                if (err)
-                return res.status(500).send(err)
-                res.send('File uploaded!')
-            })
-            break
-        default:
-            console.log('page not found')
-            res.render('404_error_template.ejs')
-            break;
-    }
+app.get('/test', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/home', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.send('Hé ho, c\'est privé ici !');
+});
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+        return res.status(400).send('No files were uploaded.')
+    let sampleFile = req.files.sampleFile
+    console.log(__dirname)
+    sampleFile.mv(__dirname + '/Notes-unparsed/' + req.files.sampleFile.name, function(err) {
+        console.log({err})
+        if (err)
+            return res.status(500).send(err)
+        res.render('page_upload.ejs')
+    })
 })
 
-app.listen(port)
+app.get('/read', (req, res) => {
+    fs.readFile(__dirname + '/Notes-unparsed/' + 'Notes Sapiens.html', 'utf8', function(err, html){
+        if (!err) {
+            const dom = parser.parseFromString(html, 'text/html');
+            const tab = html.match(/noteText'>(.+)</gm)
+            const test = tab.map(o => o.match(/>(.+)</gm)[0].match(/[^<>]+/gm)[0])
+            let body = ''
+            test.forEach(element => {
+                body += `${element}<br>`
+            })
+            res.send(body)
+        }
+    })
+})
+
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(404).send('Page introuvable !');
+});
+
+app.listen(port);
