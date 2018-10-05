963015b8d58b07d8cba5407775ee765d64ccf32a
gahubaul <gautier.hubault@gmail.com>
2018-10-06 01:59:21 +0200

    modif

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ const express = require('express')
 const path = require('path')
 const multer = require('multer')
 const fileUpload = require('express-fileupload');
+const DomParser = require('dom-parser');
+const fs = require('fs')
+const lib = require('./server/module')
+const serveStatic = require('serve-static')
+
+
 const upload = multer()
 const app = express()
 const port = process.env.PORT || 8080;
-const DomParser = require('dom-parser');
-const fs = require('fs')
-const lib = require('./module')
-
 
 
 app.use(fileUpload())
-// app.use(express.static(path.join(__dirname, '../public')))
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-    const way = path.join(__dirname, '../views/page.ejs')
+    const way = path.join(__dirname, 'views/page.ejs')
     lib.sendFile(req, res, way)
 })
 
@@ -35,13 +38,12 @@ app.post('/upload', function(req, res) {
     sampleFile.mv(__dirname + '/Notes-unparsed/' + req.files.sampleFile.name, function(err) {
         if (err)
             return res.status(500).send(err)
-        lib.sendFile(req, res, '../views/page_parsing_success.ejs')
+        lib.sendFile(req, res, 'views/page_parsing_success.ejs')
     })
 })
 
 app.get('/read', (req, res) => {
-
-    const file = path.join(__dirname, '../Notes-unparsed/Notes Sapiens.html')
+    const file = path.join(__dirname, '/Notes-unparsed/Notes Sapiens.html')
     fs.readFile(file, 'utf8', function(err, html){
         if (!err) {
             const tab = html.match(/noteText'>(.+)</gm)
@@ -59,7 +61,7 @@ app.get('/read', (req, res) => {
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/html')
     res.status(404)
-    const way = path.join(__dirname, '../views/404_error_template.ejs')
+    const way = path.join(__dirname, 'views/404_error_template.ejs')
     lib.sendFile(req, res, way)
 })
 
